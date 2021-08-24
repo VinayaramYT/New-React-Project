48b60fc95908d44de46f58b4a628617a74b606b9
VinayaramYT <86592483+VinayaramYT@users.noreply.github.com>
2021-08-24 19:13:35 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-
-import Movies from './Movies/Movies.jsx';
+import Caluclator from './Caluclator/Caluclator';
 function App() {
   return (
-    <><div className="App">
-      <p>
-        Hello World
-      </p>
+<div className="App">
+     <Caluclator />
     </div>
-    <Movies /></>
+
   );
 }
 
